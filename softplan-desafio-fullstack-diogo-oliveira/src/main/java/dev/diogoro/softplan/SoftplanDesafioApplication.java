9bf9288d60c5ebe15661132737a3ba8aa4a911b0
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-08-06 22:04:55 -0300

    versao inicial do projeto backend

---
+++ softplan-desafio-fullstack-diogo-oliveira/src/main/java/dev/diogoro/softplan/SoftplanDesafioApplication.java
@@ -0,0 +1,13 @@
+package dev.diogoro.softplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplanDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplanDesafioApplication.class, args);
+	}
+
+}
---
+++ softplan-desafio-fullstack-diogo-oliveira/src/test/java/dev/diogoro/softplan/SoftplanDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diogoro.softplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplanDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
