c45d7c914ed65a2f96debf837d69d024ae56927a
Diogo Ribeiro de Oliveira <diogo.ribo@gmail.com>
2020-08-09 18:09:52 -0300

    Correção no mapeamento dos perfis

--- softplan-desafio-fullstack-diogo-oliveira/src/main/java/dev/diogoro/softplan/domain/Usuario.java
+++ softplan-desafio-fullstack-diogo-oliveira/src/main/java/dev/diogoro/softplan/domain/Usuario.java
@@ -5,9 +5,11 @@ import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -49,8 +51,9 @@ public class Usuario {
 	private String nome;
 	
 	@NotNull
+	@ElementCollection(fetch = FetchType.EAGER, targetClass = Perfil.class)
 	@Enumerated(EnumType.STRING)
-	private Set<Perfil> perfis; //Trocar por enum
+	private Set<Perfil> perfis;
 	
 	@OneToMany
 	private Set<Processo> processosCriado;
